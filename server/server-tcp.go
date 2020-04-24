62af33824382b19acc22f5af17c71e369765b60c
Uday Hiwarale <Uday.Hiwarale>
2020-04-24 23:01:09 +0530

    TCP example added

---
+++ server/server-tcp.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net"
+	"net/rpc"
+
+	"github.com/course-one/go-rpc-starter-kit/common"
+)
+
+func main() {
+
+	// create a `*College` object
+	mit := common.NewCollege()
+
+	// create a custom RPC server
+	server := rpc.NewServer()
+
+	// register `mit` object with `server`
+	server.Register(mit)
+
+	// create a TCP listener at address : 127.0.0.1:9002
+	// https://golang.org/pkg/net/#Listener
+	listener, _ := net.Listen("tcp", "127.0.0.1:9002")
+
+	for {
+
+		// get connection from the listener when client connects
+		conn, _ := listener.Accept() // Accept blocks until next connection is received
+
+		// serve connection in a separate goroutine
+		go server.ServeConn(conn)
+	}
+
+}
